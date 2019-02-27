f01cb9be613c9d96028d2882bd8c26e3232e84dc
Seungjun Lee <seung9779@gmail.com>
2019-02-27 21:01:36 +0900

    delete $OPTS

--- replacekey.sh
+++ replacekey.sh
@@ -4,8 +4,8 @@ CURRENT_DIR=$PWD
 cd crypto-config/peerOrganizations/org1.example.com/ca/
 PRIV_KEY=$(ls *_sk)
 cd "$CURRENT_DIR"
-sed $OPTS "s/CA1_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-pc4.yaml
+sed "s/CA1_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-pc4.yaml
 cd crypto-config/peerOrganizations/org2.example.com/ca/
 PRIV_KEY=$(ls *_sk)
 cd "$CURRENT_DIR"
-sed $OPTS "s/CA2_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-pc5.yaml
+sed "s/CA2_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-pc5.yaml
