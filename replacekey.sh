cbe866f6eb4464b0960d61a4f80c8e95eb7b04d6
Seungjun Lee <seung9779@gmail.com>
2019-02-27 20:58:33 +0900

    commit

--- replacekey.sh
+++ replacekey.sh
@@ -1,14 +1,11 @@
+#!/bin/bash
+
 CURRENT_DIR=$PWD
 cd crypto-config/peerOrganizations/org1.example.com/ca/
 PRIV_KEY=$(ls *_sk)
 cd "$CURRENT_DIR"
-sed $OPTS "s/CA1_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-org1.yaml
+sed $OPTS "s/CA1_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-pc4.yaml
 cd crypto-config/peerOrganizations/org2.example.com/ca/
 PRIV_KEY=$(ls *_sk)
 cd "$CURRENT_DIR"
-sed $OPTS "s/CA2_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-org2.yaml
-# If MacOSX, remove the temporary backup of the docker-compose file
-if [ "$ARCH" == "Darwin" ]; then
-  rm docker-compose-org1.yamlt
-  rm docker-compose-org2.yamlt
-fi
+sed $OPTS "s/CA2_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-pc5.yaml
