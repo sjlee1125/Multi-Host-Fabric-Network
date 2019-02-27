70bdaa6ca24f57497e193961f10c100fd41b8750
Seungjun Lee <seung9779@gmail.com>
2019-02-27 21:05:56 +0900

    add $OPTS

--- replacekey.sh
+++ replacekey.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+ARCH=$(uname -s | grep Darwin)
+if [ "$ARCH" == "Darwin" ]; then
+ OPTS="-it"
+else
+ OPTS="-i"
+fi
+
 CURRENT_DIR=$PWD
 cd crypto-config/peerOrganizations/org1.example.com/ca/
 PRIV_KEY=$(ls *_sk)
 cd "$CURRENT_DIR"
-sed "s/CA1_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-pc4.yaml
+sed $OPTS "s/CA1_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-pc4.yaml
 cd crypto-config/peerOrganizations/org2.example.com/ca/
 PRIV_KEY=$(ls *_sk)
 cd "$CURRENT_DIR"
-sed "s/CA2_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-pc5.yaml
+sed $OPTS "s/CA2_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-pc5.yaml
